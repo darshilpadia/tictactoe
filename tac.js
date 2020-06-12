9a31d2e5e5d807e5829ac5926e12ae679fb42e01
darshilpadia <60467397+darshilpadia@users.noreply.github.com>
2020-06-12 11:28:48 +0530

    updated

--- tac.js
+++ tac.js
@@ -1,12 +1,13 @@
 const wincomb=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-let hplayer;
-let c=0,f=0,t=0;
+let hplayer="0";
+let c=0,f=0,t=0,temp=0;
 class game{
 newgame()
 {
 	hplayer="0";
 	c=0;
 	f=0;
+	temp=0;
 	document.getElementById("endgame").style.visibility = "hidden";
 	document.getElementById("player").innerText=hplayer+"'s turn";
 
@@ -17,7 +18,9 @@ newgame()
 }
 showtable()
 {
+	document.getElementById("t2").style.visibility = "hidden";
 	document.getElementById("t1").style.visibility = "visible";
+	
 	play.newgame()
 	
 }
@@ -47,6 +50,19 @@ getbox(index)
 {
 	return document.getElementById("r"+index).innerText;
 }
+emptybox()
+{
+	let boxindex=[]
+	for(let k=1;k<10;k++)
+	{
+		if(document.getElementById("r"+k).innerText == "")
+		{
+			boxindex.push(k);
+		}
+			
+	}
+	console.log(boxindex);
+}
 
 }
 
@@ -61,21 +77,21 @@ class multiplayer extends game
 				document.getElementById("t1").style.visibility = "hidden";
 				document.getElementById("endgame").style.visibility = "visible";
 				document.getElementById("endgame").innerText=hplayer+" win!";
-				break;	
+				temp=1;
 			}
-			else if(c==9)
-			{	
-				document.getElementById("t1").style.visibility = "hidden";
-				document.getElementById("endgame").style.visibility = "visible";
-				document.getElementById("endgame").innerText="Tie Game";
-			}
-				
+			
+		}
+		if(temp==0 && c==9)
+		{
+					document.getElementById("t1").style.visibility = "hidden";
+					document.getElementById("endgame").style.visibility = "visible";
+					document.getElementById("endgame").innerText="Tie Game";
 		}
 	}
 }
 class singleplayer extends game
 {
-	msg()
+msg()
 	{
 		document.getElementById("endgame").style.visibility = "visible";
 		document.getElementById("endgame").innerText="work in progress,you can play Multiplayer";
@@ -83,6 +99,30 @@ class singleplayer extends game
 		document.getElementById("player").style.visibility = "hidden";
 		
 	}
+showtable()
+{
+	document.getElementById("t1").style.visibility = "hidden";
+	document.getElementById("t2").style.visibility = "visible";
+	play.newgame()
+	
+}
+printtext(index)
+{
+	let boxid = document.getElementById("r"+index);
+	console.log(boxid);
+	if(boxid.innerText==""){
+		boxid.innerText=hplayer;
+		c++;
+		console.log(c);
+		/*if(c>=5)
+		{
+			multiplay.winner(hplayer);
+			
+		}
+		play.emptybox();
+		document.getElementById("player").innerText=hplayer+"'s turn";*/
+	}
+}
 }
 let play = new game();
 let multiplay = new multiplayer();
